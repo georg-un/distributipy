03d9621fb13e7da2320ff3dc2e2610db68e1760b
georg_un <onkovic@gmail.com>
2019-06-22 21:11:40 +0200

    Remove theoretical distribution levy_stable
    The new version of scipy.stats causes levy_stable to take a very long time to be fitted. Therefore, it will temporarly be removed.

--- distributipy/distribution_fitter.py
+++ distributipy/distribution_fitter.py
@@ -112,7 +112,7 @@ class DistributionFitter:
 
     def best_n_fitting(self, n=5, verbose=True):
         """
-        Fit 89 scipy.stats distributions to the data. Return the n best distributions in terms of their SSE.
+        Fit 88 scipy.stats distributions to the data. Return the n best distributions in terms of their SSE.
 
         :param n:       Integer (at least 1). Defines the number of distributions to return.
 
@@ -347,7 +347,7 @@ def _get_distributions():
         st.gennorm, st.genexpon, st.genextreme, st.gausshyper, st.gamma, st.gengamma, st.genhalflogistic, st.gilbrat,
         st.gompertz, st.gumbel_r, st.gumbel_l, st.halfcauchy, st.halflogistic, st.halfnorm, st.halfgennorm,
         st.hypsecant, st.invgamma, st.invgauss, st.invweibull, st.johnsonsb, st.johnsonsu, st.ksone, st.kstwobign,
-        st.laplace, st.levy, st.levy_l, st.levy_stable, st.logistic, st.loggamma, st.loglaplace, st.lognorm, st.lomax,
+        st.laplace, st.levy, st.levy_l, st.logistic, st.loggamma, st.loglaplace, st.lognorm, st.lomax,
         st.maxwell, st.mielke, st.nakagami, st.ncx2, st.ncf, st.nct, st.norm, st.pareto, st.pearson3, st.powerlaw,
         st.powerlognorm, st.powernorm, st.rdist, st.reciprocal, st.rayleigh, st.rice, st.recipinvgauss, st.semicircular,
         st.t, st.triang, st.truncexpon, st.truncnorm, st.tukeylambda, st.uniform, st.vonmises, st.vonmises_line,
