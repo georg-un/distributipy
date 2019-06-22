aa4e0c427d3fb88c61fc3dbc5a89e1e9e7178545
georg_un <onkovic@gmail.com>
2019-06-23 00:40:47 +0200

    Add test-data for unit-tests

--- distributipy/tests/test_distribution_fitter.py
+++ distributipy/tests/test_distribution_fitter.py
@@ -1,14 +1,17 @@
 import unittest
+import os
 
 from distributipy.distribution_fitter import *
+
+data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
 
 class TestDistributionFitter(unittest.TestCase):
 
     def test_fit_single_distribution(self):
-        data = np.random.normal(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distribution = distribution_fitter.fit(st.norm)
+        data = np.genfromtxt(os.path.join(data_path, "normal.csv"), delimiter=",")
 
         self.assertIsNotNone(fitted_distribution)
         self.assertEqual(fitted_distribution.distribution, st.norm)
@@ -18,51 +21,51 @@ class TestDistributionFitter(unittest.TestCase):
         self.assertNotEqual(fitted_distribution.sse, 0.0)
 
     def test_normal_distribution_probability_x_less(self):
-        data = np.random.normal(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distribution = distribution_fitter.fit(st.norm)
+        data = np.genfromtxt(os.path.join(data_path, "normal.csv"), delimiter=",")
 
         self.assertAlmostEqual(fitted_distribution.probability_x_less_equal(250), 0.5, delta=10)
         self.assertGreater(fitted_distribution.probability_x_less_equal(300), 0.95)
         self.assertLess(fitted_distribution.probability_x_less_equal(200), 0.05)
 
     def test_laplace_distribution_probability_x_less(self):
-        data = np.random.laplace(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distribution = distribution_fitter.fit(st.norm)
+        data = np.genfromtxt(os.path.join(data_path, "laplace.csv"), delimiter=",")
 
         self.assertAlmostEqual(fitted_distribution.probability_x_less_equal(250), 0.5, delta=10)
         self.assertGreater(fitted_distribution.probability_x_less_equal(300), 0.95)
         self.assertLess(fitted_distribution.probability_x_less_equal(200), 0.05)
 
     def test_normal_distribution_probability_x_greater(self):
-        data = np.random.normal(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distribution = distribution_fitter.fit(st.norm)
+        data = np.genfromtxt(os.path.join(data_path, "normal.csv"), delimiter=",")
 
         self.assertAlmostEqual(fitted_distribution.probability_x_greater_equal(250), 0.5, delta=10)
         self.assertLess(fitted_distribution.probability_x_greater_equal(300), 0.05)
         self.assertGreater(fitted_distribution.probability_x_greater_equal(200), 0.95)
 
     def test_normal_distribution_probability_for_x(self):
-        data = np.random.normal(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distribution = distribution_fitter.fit(st.norm)
+        data = np.genfromtxt(os.path.join(data_path, "normal.csv"), delimiter=",")
 
         self.assertAlmostEqual(fitted_distribution.value_for_probability_x(0.5), 250, delta=10)
 
     def test_find_single_best_fitting_distribution(self):
-        data = np.random.normal(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distributions = distribution_fitter.best_n_fitting(1)
+        data = np.genfromtxt(os.path.join(data_path, "normal.csv"), delimiter=",")
 
         self.assertIsNotNone(fitted_distributions.distributions)
         self.assertEqual(len(fitted_distributions.distributions), 1)
 
     def test_find_n_best_fitting_distributions(self):
-        data = np.random.normal(loc=250, scale=5, size=500)
         distribution_fitter = DistributionFitter(data=data, n_bins=125)
         fitted_distributions = distribution_fitter.best_n_fitting(89)
+        data = np.genfromtxt(os.path.join(data_path, "normal.csv"), delimiter=",")
 
         self.assertIsNotNone(fitted_distributions.distributions)
         self.assertAlmostEqual(len(fitted_distributions.distributions), 89, delta=5)
